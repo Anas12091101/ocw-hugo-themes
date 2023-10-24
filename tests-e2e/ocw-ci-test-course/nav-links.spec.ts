c1261a9ea93d0e1b34a4bac1cdcd4b034f92ffc6
Muhammad Anas <muhammad.anas@A006-00605.local>
2023-10-24 19:31:50 +0500

    removed unwanted import

---
+++ tests-e2e/ocw-ci-test-course/nav-links.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test"
+import { CoursePage } from "../util"
+
+test("Navbar links working properly", async ({ page }) => {
+  const course = new CoursePage(page, "course")
+  await course.goto("/")
+
+  const navbar = await page
+    .locator("#course-main-content div")
+    .filter({
+      hasText:
+        "Section 1 Menu Title Subsection 1a Menu Title Subsection 1b Menu Title Section 2",
+    })
+    .nth(3)
+  const linkCount = await navbar.locator("a").count()
+  for (let i = 0; i < linkCount; i++) {
+    const link = await navbar.locator("a").nth(i)
+    const url = await link.getAttribute("href")
+    await link.click()
+    await expect(page.url()).toContain(url)
+  }
+})
+
+test("Navbar Expanding Properly", async ({ page }) => {
+  const course = new CoursePage(page, "course")
+  await course.goto("/")
+
+  const expandBtn = page.getByRole("button", {
+    name: "Subsections for Section 1 Menu Title",
+  })
+  await expandBtn.click()
+  const subHeading = await page.getByRole("link", {
+    name: "Subsection 1a Menu Title",
+  })
+  await expect(subHeading).toBeVisible()
+  await expandBtn.click()
+  await expect(subHeading).toBeHidden()
+})
